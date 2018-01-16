0bc84fd1268a6a618c6fe0439875a7fe540525f1
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2018-01-16 13:06:41 +0100

    fix variable to vcl

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ start_varnishd () {
 
 }
 
-if [ ${VARNISH_VCL_CUSTOM_PATH} == "YOUR-VCL-PATH" ]; then
+if [ ${VARNISH_VCL_PATH} == "YOUR-VCL-PATH" ]; then
 
     echo "You did not configure your vcl-path properly."
     echo "See docs for configuration-exmaple."
@@ -29,12 +29,12 @@ if [ ${VARNISH_VCL_CUSTOM_PATH} == "YOUR-VCL-PATH" ]; then
 
 fi
 
-if ! [ -s "${VARNISH_VCL_CUSTOM_PATH}" ]; then
+if ! [ -s "${VARNISH_VCL_PATH}" ]; then
 
     echo "Provided VCL is either empty or not existent:"
-    echo "${VARNISH_VCL_CUSTOM_PATH}"
+    echo "${VARNISH_VCL_PATH}"
     exit 1
 
 fi
 
-start_varnishd "${VARNISH_VCL_CUSTOM_PATH}"
+start_varnishd "${VARNISH_VCL_PATH}"
